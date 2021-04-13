99710477dba18e3945f103b0b0a1fa30755fb2a5
toyabev37 <toyabev37@gmail.com>
2021-04-13 11:48:10 -0400

    ItitPush

---
+++ TwentyFour.Data/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyFour.Data
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public virtual List<Comment> Comments { get; set; }      
+        public Guid AuthorId { get; set; }
+
+    }
+}
