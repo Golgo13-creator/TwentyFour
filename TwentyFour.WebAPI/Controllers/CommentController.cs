f7e356269b954c0cbfb26f73385618dcbde2ccd0
laamaj92@gmail.com <laamaj92@gmail.com>
2021-04-13 21:11:34 +0200

    controller initial commit

---
+++ TwentyFour.WebAPI/Controllers/CommentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TwentyFour.WebAPI.Controllers
+{
+    [Authorize]
+    public class CommentController : ApiController
+    {
+        private CommentService CreateCommentService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new CommentService(userId);
+            return postService;
+        }
+        public IHttpActionResult Post(CommentCreate comment)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateCommentService();
+            if (!service.CreateComment(comment))
+                return InternalServerError();
+            return Ok();
+        }
+        public IHttpActionResult Get(int id)
+        {
+            CommentService commentService = CreateCommentService();
+            var posts = commentService.GetPost();
+            return Ok(posts);
+        }
+    }
+}
